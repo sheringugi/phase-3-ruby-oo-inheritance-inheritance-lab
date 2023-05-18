a08a1eb58ce61d66a06f1b7054f0ae935aa46771
sheringugi <sheri.ngugi17@gmail.com>
2023-05-19 01:40:54 +0300

    complete assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user'
 
+class Student < User  
+  def initialize(first_name, last_name)
+    super(first_name, last_name) 
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
